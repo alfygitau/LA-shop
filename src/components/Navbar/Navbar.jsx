0b2b2486168a05bd94f24e9d367a8c5f664b8e78
alfygitau <alfygitau@gmail.com>
2022-03-16 12:17:07 +0300

    fixed the navbar css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+        <div className="container">
+          <a className="navbar-brand fw-bold fs-4" href="/">
             LA COLLECTION
           </a>
           <button
@@ -20,7 +20,7 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   Home
@@ -28,58 +28,34 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/">
-                  Link
+                  Products
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" href="/">
-                  Disabled
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Contact
                 </a>
               </li>
             </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+            <div className="buttons">
+              <a href="/" className="btn btn-outline-dark">
+                {" "}
+                <i className="fa fa-sign-in me-1"></i> Login
+              </a>
+              <a href="/" className="btn btn-outline-dark ms-2">
+                {" "}
+                <i className="fa fa-user-plus"></i> Register
+              </a>
+              <a href="/" className="btn btn-outline-dark ms-2">
+                {" "}
+                <i className="fa fa-shopping-cart"></i> Cart (0)
+              </a>
+            </div>
           </div>
         </div>
       </nav>
