197753c286e1cf31e27633a7e30dcba47ec294c8
alfygitau <alfygitau@gmail.com>
2022-03-29 20:30:26 +0300

    added a button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteCart } from "../../Redux/actions";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const cart = useSelector((state) => state.cartReducer);
   const dispatch = useDispatch();
   console.log("cart", cart);
+  const navigate = useNavigate();
 
   const removeProduct = (product) => {
     dispatch(deleteCart(product));
@@ -25,34 +27,50 @@ const Cart = () => {
             <th scope="col">Actions</th>
           </tr>
         </thead>
-        {cart.length < 1 ? <h1>Cart Empty</h1> : cart.map((product) => (
-          <tbody key={product.id}>
-            <tr>
-              <th scope="row">{product.id}</th>
-              <td>
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  height="40px"
-                  width="40px"
-                />
-              </td>
-              <td>{product.title}</td>
-              <td>$ {product.price}</td>
-              <td>{product.quantity}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => removeProduct(product)}
-                >
-                  Remove
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        ))}
+        {cart.length < 1 ? (
+          <>
+            <h4>Cart is currently empty</h4>
+            <button
+              className="btn btn-outline-dark"
+              onClick={() => navigate("/products")}
+            >
+              Continue Shopping
+            </button>
+          </>
+        ) : (
+          cart.map((product) => (
+            <tbody key={product.id}>
+              <tr>
+                <th scope="row">{product.id}</th>
+                <td>
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    height="40px"
+                    width="40px"
+                  />
+                </td>
+                <td>{product.title}</td>
+                <td>$ {product.price}</td>
+                <td>{product.quantity}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeProduct(product)}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          ))
+        )}
       </table>
-      <button className="btn btn-outline-dark">Proceed to checkout</button>
+      {cart.length >= 1 ? (
+        <button className="btn btn-outline-dark">Proceed to checkout</button>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
